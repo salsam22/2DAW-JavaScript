abf64095de08d1fa171ba2cddc1a9836ae81586a
salsam22 <76152023+salsam22@users.noreply.github.com>
2021-11-02 13:32:20 +0100

    JS

--- Tema5/ExpresionesRegulares/ExpresionesRegulares.js
+++ Tema5/ExpresionesRegulares/ExpresionesRegulares.js
@@ -5,99 +5,80 @@ function iniciar() {
 }
 
 function validarDNI() {
+    var patternDNI = new RegExp(/^\d{8}[A-Z]{1}$/);
     var element = document.getElementById("DNI");
-    if (!element.checkValidity()) {
-        if (element.validity.valueMissing) {
-            error2(element, "Tienes que introducir un DNI.");
-        }
-        if (element.validity.patternMismatch) {
-            error2(element, "El DNI tiene que tener el formato 99999999Z.");
-        }
-        //error(element);
+    if (patternDNI.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "Error: DNI incompleto o invalido.");
         return false;
     }
-    return true;
 }
 
 function validarTel() {
+    var patternTel = new RegExp(/^\d{3}\s\d{3}\s\d{3}$$/);
     var element = document.getElementById("telefono");
-    if (!element.checkValidity()) {
-        if (element.validity.valueMissing) {
-            error2(element, "Tienes que introducir un telefono.");
-        }
-        if (element.validity.patternMismatch) {
-            error2(element, "El telefono tiene que tener el formato 999 999 999.");
-        }
-        //error(element);
+    if (patternTel.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "Error: Telefono incompleto o invalido.");
         return false;
     }
-    return true;
 }
 
 function validarFecha() {
+    var patternFecha = new RegExp(/^\d{4}\/(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])$/);
     var element = document.getElementById("fecha");
-    if (!element.checkValidity()) {
-        if (element.validity.valueMissing) {
-            error2(element, "Tienes que introducir una fecha.");
-        }
-        if (element.validity.patternMismatch) {
-            error2(element, "La fecha tiene que tener el formato YYYY/mm/dd.");
-        }
+    if (patternFecha.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "La fecha tiene que tener el formato YYYY/mm/dd.");
         return false;
     }
-    return true;
 }
 
 function validarMatr() {
+    var patternMatr = new RegExp(/^\d{4}\s[A-Z]{3}$/);
     var element = document.getElementById("matricula");
-    if (!element.checkValidity()) {
-        if (element.validity.valueMissing) {
-            error2(element, "Tienes que introducir una matricula.");
-        }
-        if (element.validity.patternMismatch) {
-            error2(element, "La matricula tiene que tener el formato 9999 ZZZ.");
-        }
+    if (patternMatr.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "La matricula tiene que tener el formato 9999 ZZZ.");
         return false;
     }
-    return true;
 }
 
 function validarEmail() {
+    var patternEmail = new RegExp(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/);
     var element = document.getElementById("email");
-    if (!element.checkValidity()) {
-        if (element.validity.valueMissing) {
-            error2(element, "Tienes que introducir un email.");
-        }
-        if (element.validity.patternMismatch) {
-            error2(element, "El email tiene que estar correctamente escrito.");
-        }
+    if (patternEmail.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "El email tiene que estar correctamente escrito.");
         return false;
     }
-    return true;
 }
 
 function validarURL() {
+    var patternURL = new RegExp(/^(ftp|http|https)[^ "]+$/);
     var element = document.getElementById("URL");
-    if (!element.checkValidity()) {
-        if (element.validity.valueMissing) {
-            error2(element, "Tienes que introducir una URL.");
-        }
-        if (element.validity.patternMismatch) {
-            error2(element, "El URL tiene que estar correctamente escrito.");
-        }
+    if (patternURL.test(element.value)) {
+        return true;
+    } else {
+        error2(element, "El URL tiene que estar correctamente escrito.");
         return false;
     }
-    return true;
 }
 
 function validar(e) {
     borrarError();
-    if (validarDNI() && validarTel() && validarFecha() && validarMatr() && validarEmail() && validarURL() && confirm("Confirma si vols enviar el formulari")) {
+    if (validarDNI() && validarTel() && validarFecha() && validarMatr() && validarEmail() && validarURL() && confirm("hola")) {
+        console.log("hola");
         return true;
     } else {
         e.preventDefault();
         return false;
-    }
+    } console.log("hola");
 }
 
 function error2(element, missatge) {

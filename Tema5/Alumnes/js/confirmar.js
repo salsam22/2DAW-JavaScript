a06d7635eed94b4998ea9610934e75a55699d22c
salsam22 <76152023+salsam22@users.noreply.github.com>
2021-11-18 12:01:03 +0100

    JS

--- Tema5/Alumnes/js/confirmar.js
+++ Tema5/Alumnes/js/confirmar.js
@@ -9,44 +9,51 @@ function inicio() {
 }
 
 function mostrar() {
+    var pare = document.getElementById("articulos");
     arrayProductos.forEach((element,index) => {
-    var primerDiv = document.createElement("div");
-    primerDiv.setAttribute("class", "card mt-2");
-    primerDiv.setAttribute("style", "width: 25rem;");
-    var h5 = document.createElement("h5");
-    h5.setAttribute("class", "card-header");
-    var nombreProducto = document.createTextNode(element.nombre);
-    h5.appendChild(nombreProducto);
-    var segundoDiv = document.createElement("div");
-    segundoDiv.setAttribute("class", "card-body");
-    var tercerDiv = document.createElement("div");
-    tercerDiv.setAttribute("class", "row");
-    var quartoDiv = document.createElement("div");
-    quartoDiv.setAttribute("class", "col");
-    var h3 = document.createElement("h3");
-    h3.setAttribute("class", "card-title");
-    var precioProducto = document.createTextNode(element.precio + " €");
-    h3.appendChild(precioProducto);
-    var p = document.createElement("p");
-    p.setAttribute("class", "card-text");
-    var tallaProducto = document.createTextNode("Talla " + element.talla);
-    p.appendChild(tallaProducto);
-    var a = document.createElement("a");
-    a.setAttribute("href", "#");
-    a.setAttribute("class", "btn btn-primary text-end");
-    var i = document.createElement("i");
-    i.setAttribute("class", "fa fa-trash-o");
-    i.setAttribute("aria-hidden", "true")
-    a.appendChild(i);
-    quartoDiv.appendChild(h3);
-    quartoDiv.appendChild(p);
-    quartoDiv.appendChild(a);
-    var quintoDiv = document.createElement("div");
-    quintoDiv.setAttribute("class", "col");
-    var img = document.createElement("img");
-    img.setAttribute("src", "./img/" + element.imagen)
-    img.setAttribute("class", "img-fluid img-thumbnail")
-    quintoDiv.appendChild(img);
+        var primerDiv = document.createElement("div");
+        primerDiv.setAttribute("class", "card mt-2");
+        primerDiv.setAttribute("style", "width: 25rem;");
+        var h5 = document.createElement("h5");
+        h5.setAttribute("class", "card-header");
+        var nombreProducto = document.createTextNode(element.nombre);
+        h5.appendChild(nombreProducto);
+        var segundoDiv = document.createElement("div");
+        segundoDiv.setAttribute("class", "card-body");
+        var tercerDiv = document.createElement("div");
+        tercerDiv.setAttribute("class", "row");
+        var quartoDiv = document.createElement("div");
+        quartoDiv.setAttribute("class", "col");
+        var h3 = document.createElement("h3");
+        h3.setAttribute("class", "card-title");
+        var precioProducto = document.createTextNode(element.precio + " €");
+        h3.appendChild(precioProducto);
+        var p = document.createElement("p");
+        p.setAttribute("class", "card-text");
+        var tallaProducto = document.createTextNode("Talla " + element.talla);
+        p.appendChild(tallaProducto);
+        var a = document.createElement("a");
+        a.setAttribute("href", "#");
+        a.setAttribute("class", "btn btn-primary text-end");
+        var i = document.createElement("i");
+        i.setAttribute("class", "fa fa-trash-o");
+        i.setAttribute("aria-hidden", "true")
+        a.appendChild(i);
+        quartoDiv.appendChild(h3);
+        quartoDiv.appendChild(p);
+        quartoDiv.appendChild(a);
+        var quintoDiv = document.createElement("div");
+        quintoDiv.setAttribute("class", "col");
+        var img = document.createElement("img");
+        img.setAttribute("src", "./img/" + element.imagen)
+        img.setAttribute("class", "img-fluid img-thumbnail")
+        quintoDiv.appendChild(img);
+        tercerDiv.appendChild(quartoDiv);
+        tercerDiv.appendChild(quintoDiv);
+        segundoDiv.appendChild(tercerDiv);
+        primerDiv.appendChild(h5);
+        primerDiv.appendChild(segundoDiv);
+        pare.appendChild(primerDiv);
     
     });
 }

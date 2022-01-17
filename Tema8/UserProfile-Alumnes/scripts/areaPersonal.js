af97c660b72de765e43590d03abca6dd5fc08445
salsam22 <76152023+salsam22@users.noreply.github.com>
2022-01-17 12:05:24 +0100

    JS

--- Tema8/UserProfile-Alumnes/scripts/areaPersonal.js
+++ Tema8/UserProfile-Alumnes/scripts/areaPersonal.js
@@ -2,6 +2,8 @@ window.onload = inicio;
 
 function inicio() {
     mostrarNombre();
+    mostrarUsuario();
+    document.getElementById("enviar").addEventListener("click", validar, false);
 }
 
 function mostrarNombre() {
@@ -16,6 +18,7 @@ function mostrarNombre() {
     .then(data => {
         if(data.error == null){
         console.log("FUNCIONA");
+        console.log(data)
         let nom = document.getElementById("nom");
         nom.value = data.data.user.name;
         }else{
@@ -26,4 +29,129 @@ function mostrarNombre() {
     .catch((error) => {
         console.error('Error:', error);
     })
+}
+
+function mostrarUsuario() {
+    fetch('https://userprofile.serverred.es/api/areapersonal', {
+        method: 'GET',
+        headers: {
+            'Accept' : 'application/json',
+            "auth-token": JSON.parse(localStorage.getItem("TK"))
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.error == null){
+        console.log("FUNCIONA");
+        let nom = document.getElementById("user");
+        nom.innerHTML = data.data.user.name;
+        }else{
+        console.log("ERROR");
+        console.log(data.error);
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function validarPassword() {
+    esborrarError();
+    var element = document.getElementById("passworda");
+    if (!element.checkValidity()) {
+        if (element.validity.valueMissing) {
+            error2(element, "Error: La contraseña es requerida.");
+        }
+        if (element.validity.patternMismatch) {
+            error2(element, "Error: La contraseña tiene que tener mas de 6 caracteres.")
+        }
+        return false;
+    }
+    passwd = element.value;
+    borrarError();
+    return true;
+}
+
+function validarPasswordNova() {
+    esborrarError();
+    var element = document.getElementById("password");
+    if (!element.checkValidity()) {
+        if (element.validity.valueMissing) {
+            error2(element, "Error: La contraseña es requerida.");
+        }
+        if (element.validity.patternMismatch) {
+            error2(element, "Error: La contraseña tiene que tener mas de 6 caracteres.")
+        }
+        return false;
+    }
+    passwd = element.value;
+    borrarError();
+    return true;
+}
+
+function validarPasswordc() {
+    esborrarError();
+    var element = document.getElementById("passwordc");
+    var passwd = document.getElementById("password")
+    console.log(passwd.value);
+    console.log(element.value);
+    if (element.value != passwd.value) {
+        error2(element, "Error: La repetición de la contraseña tiene que ser igual que la contraseña introducida anteriormente.")
+        return false;
+    }
+    borrarError();
+    return true;
+}
+
+function validar(e) {
+    e.preventDefault();
+    if (validarPassword() && validarPasswordNova() && validarPasswordc() && confirm("¿Seguro que quieres registrarte?")) {
+        actualizarAPI();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function error2(element, missatge) {
+    document.getElementById("missatgeError").innerHTML = missatge;
+    element.className = "form-control border-danger";
+    element.focus();
+}
+
+function esborrarError() {
+    var formulari = document.forms[0];
+    for (var i = 0; i < formulari.elements.length - 1; i++) {
+        formulari.elements[i].className = "form-control";
+    }
+}
+
+function borrarError() {
+    document.getElementById("missatgeError").innerHTML = "";
+}
+
+function actualizarAPI() {
+    var newPass = {
+        "name": document.getElementById("nom").value,
+        "password": document.getElementById("passworda").value,
+        "newPassword": document.getElementById("password").value
+    }
+    console.log(newPass);
+    fetch(" https://userprofile.serverred.es/api/areaPersonal", {
+        method:"PUT",
+        headers: {
+            "Content-Type": "application/json",
+            'Accept' : 'application/json',
+            "auth-token": JSON.parse(localStorage.getItem("TK"))
+        },
+        body:JSON.stringify(newPass)
+    })
+        .then(response => response.json())
+        .then(data =>{ 
+            console.log(data)
+            if(data.error==null){
+                alert("S'ha actualitzat la contraseña correctament");
+            }
+        })
+        .catch(error=>console.log(error));
 }
--- Tema8/UserProfile-Alumnes/scripts/index.js
+++ Tema8/UserProfile-Alumnes/scripts/index.js
@@ -2,30 +2,6 @@ window.onload = inicio;
 
 function inicio() {
     mostrarUsuario();
-    document.getElementById("enviar").addEventListener("click", logout);
-}
-
-function logout() {
-    localStorage.removeItem("TK");
-    logged();
-}
-
-function logged() {
-    if (JSON.parse(localStorage.getItem("TK")) == null) {
-        location.assign("login.html");
-    } else {
-        fetch("https://userprofile.serverred.es/api/areapersonal", {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                "auth-token": JSON.parse(localStorage.getItem("TK"))
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            });
-    }
 }
 
 function mostrarUsuario() {
